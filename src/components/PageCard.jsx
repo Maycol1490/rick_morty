e37bac2247fd543d111305f492aa4547bcbb9dc5
Maycol1490 <maicol1490@hotmail.com>
2022-10-03 14:54:13 -0500

    s

--- src/components/PageCard.jsx
+++ src/components/PageCard.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import { useState } from 'react'
 
-const PageCard = ({page,setPage,maxPage}) => {
+const PageCard = ({ page, setPage, maxPage }) => {
     const [numberPage, setNumberPage] = useState(1)
 
-    const nextPage=()=>{
-        if(numberPage,page < maxPage){
-            setNumberPage(numberPage+1)
-            setPage(page+1)
+    const nextPage = () => {
+        if (numberPage, page < maxPage) {
+            setNumberPage(numberPage + 1)
+            setPage(page + 1)
         }
 
     }
 
-    const prevPage=()=>{
-        if(numberPage,page > 1){
-            setNumberPage(numberPage-1)
-            setPage(page-1)
+    const prevPage = () => {
+        if (numberPage, page > 1) {
+            setNumberPage(numberPage - 1)
+            setPage(page - 1)
         }
     }
-  return (
-    <div className='container_cardPage'>
-        <button onClick={prevPage} className="btn_cardPage" >prev</button>
-        <p className='number_cardPage'>{numberPage}</p>
-        <span>De</span> 
-        <p className='number_cardPage'>{maxPage}</p>
-        <button onClick={nextPage} className="btn_cardPage" >next</button>
+    return (
+        <div className='container_cardPage'>
+            <button onClick={prevPage} className="btn_cardPage" >prev</button>
+            <div className='number_cardPage'>
+            <p>{numberPage}</p>
+            <span>De</span>
+            <p>{maxPage}</p>
+            </div>        
+            <button onClick={nextPage} className="btn_cardPage" >next</button>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default PageCard
